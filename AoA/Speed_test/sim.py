3970517aaf756ecbb0d4ac13aacbda49b7128a76
Pi2 <hclaus23@student.aau.dk>
2026-04-08 08:05:14 +0200

    AoA speed test results

--- AoA/Speed_test/sim.py
+++ AoA/Speed_test/sim.py
@@ -9,14 +9,18 @@ esprit_results = []
 runtimes = []
 
 
-for _ in range(trials):
+for t in range(trials):
+    #print("Trial number: ", t)
     angle = np.random.uniform(-50, 50)
-    rx = sample_gen(2, 120e3, 200, angle, 14, 0.5)
+    rx = sample_gen(64, 120e3, 200, angle, 13, 0.5)
     t0 = time.perf_counter()
     esprit_results.append(esprit(rx, 1))
     t1 = time.perf_counter()
     runtimes.append(t1-t0)
 
-print("Average: ", np.average(runtimes))
-print("Max: ", np.max(runtimes))
-print("Min: ", np.min(runtimes))
+np.savez_compressed(
+    'esprit_benchmark_data.npz', 
+    esprit_results=np.array(esprit_results), 
+    runtimes=np.array(runtimes)
+
+)
