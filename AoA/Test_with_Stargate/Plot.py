92453e934c8d7aabd1bbd5ee08cc1da5cff3c5f8
Pi2 <hclaus23@student.aau.dk>
2026-04-14 14:38:00 +0200

    Test data from stargate test AoA

--- AoA/Test_with_Stargate/Plot.py
+++ AoA/Test_with_Stargate/Plot.py
@@ -3,15 +3,16 @@ import matplotlib.pyplot as plt
 
 # 1. Load the data
 
-data = np.load('AoA/Test_with_Stargate/usrp_data_from_degree_0.npz')
-n_zoom = 500 #Max 1000000
+
+data = np.load('/home/pi2/Desktop/Git/ESD_P6/AoA/Test_with_Stargate/usrp_data_from_degree_XX.npz')
+n_zoom = 100 #Max 1000000
 
 
 ch1 = data['ch1']
 ch2 = data['ch2']
 
 # 2. Settings (Adjust these based on your actual capture)
-fs = 100e6  # Sample rate: 100 MHz
+fs = 1e6  # Sample rate: 100 MHz
 t = np.arange(len(ch1)) / fs
 
 # 3. Create the Visualization
@@ -20,8 +21,10 @@ fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 10))
 # --- Top Plot: Time Domain (Magnitude) ---
 # We plot the first 500 samples to see the waveform clearly
  
-ax1.plot(t[:n_zoom] * 1e6, np.abs(ch1[:n_zoom]), label='Channel 1')
-ax1.plot(t[:n_zoom] * 1e6, np.abs(ch2[:n_zoom]), label='Channel 2', alpha=0.7)
+#ax1.plot(t[:n_zoom] * 1e6, np.abs(ch1[:n_zoom]), label='Channel 1')
+#ax1.plot(t[:n_zoom] * 1e6, np.abs(ch2[:n_zoom]), label='Channel 2', alpha=0.7)
+ax1.plot(t[:n_zoom] * 1e6, ch1[:n_zoom].real, label='Channel 1 (Real)')
+ax1.plot(t[:n_zoom] * 1e6, ch1[:n_zoom].imag, label='Channel 1 (Imag)', alpha=0.7)
 ax1.set_title(r'Time Domain: Signal Magnitude (First $500$ samples)')
 ax1.set_xlabel(r'Time ($\mu s$)')
 ax1.set_ylabel(r'Magnitude')
--- AoA/Test_with_Stargate/Run.py
+++ AoA/Test_with_Stargate/Run.py
@@ -1,18 +1,38 @@
 import uhd
 import numpy as np
+from Git.ESD_P6.AoA.Speed_test.DoA import esprit
 
 # Initialize the USRP
 usrp = uhd.usrp.MultiUSRP()
 
+
+
 # Parameters
-num_samps = 1000000  # 1 million samples
+num_samps = 100000  # 1 million samples
 center_freq = 5.8e9    # Center frequency 5.8GHz
 sample_rate = 1e6      # 1 Msps (Note: capture will take 1 second)
 channels = [0, 1]      # Port 0 and Port 1
-gain = 0              # dB
+gain = 30              # dB
+
+usrp.set_clock_source('internal')
+for chan in channels:
+    usrp.set_rx_antenna('RX2', chan) # Switch from TX/RX to RX2
+    usrp.set_rx_gain(gain, chan)
+    usrp.set_rx_rate(sample_rate, chan)
+    usrp.set_rx_freq(uhd.libpyuhd.types.tune_request(center_freq), chan)
 
 samples = usrp.recv_num_samps(num_samps, center_freq, sample_rate, channels, gain)
 
-np.savez_compressed('usrp_data_from_degree_XX.npz', ch1=samples[0], ch2=samples[1])
+
+set = samples[:,:200]
+print(set.shape)
+print("angle: ", esprit(set.T, len(set)))
+
+peak_signal = np.max(np.abs(samples))
+print("Max val:", peak_signal)
+n = 0.001450191
+print("Snr: ", 20 * np.log10(peak_signal / n))
+
+np.savez_compressed('Git/ESD_P6/AoA/Test_with_Stargate/usrp_data_from_degree_XX.npz', ch1=samples[0], ch2=samples[1])
 
 
