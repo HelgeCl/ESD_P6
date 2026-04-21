fbc90c2f6f8925888b0177966a7d7f90562367d3
HelgeCl <249914276+HelgeCl@users.noreply.github.com>
2026-04-21 20:20:02 +0200

    Non coherent X310

--- AoA/Test/6 X310_test/Plot_samples.py
+++ AoA/Test/6 X310_test/Plot_samples.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 # 1. Load the data
 
 
-#data = np.load('Git/ESD_P6/AoA/usrp_data_from_degree_XX.npz')
-data = np.load('Git/ESD_P6/AoA/X310_2.npz')
-n_zoom = 10000 #Max 1000000
+# data = np.load('Git/ESD_P6/AoA/usrp_data_from_degree_XX.npz')
+data = np.load('X310_1.npz')
+n_zoom = 10000  # Max 1000000
 
 
 ch1 = data['ch1']
@@ -17,28 +17,21 @@ fs = 1e6  # Sample rate: 100 MHz
 t = np.arange(len(ch1)) / fs
 
 # 3. Create the Visualization
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 10))
+plt.figure(figsize=(6, 4))
 
 # --- Top Plot: Time Domain (Magnitude) ---
 # We plot the first 500 samples to see the waveform clearly
- 
-ax1.plot(t[:n_zoom] * 1e6, ch2[:n_zoom].real, label='Channel 2 (real)')
-#ax1.plot(t[:n_zoom] * 1e6, np.abs(ch2[:n_zoom]), label='Channel 2', alpha=0.7)
-ax1.plot(t[:n_zoom] * 1e6, ch1[:n_zoom].real, label='Channel 1 (Real)')
-#ax1.plot(t[:n_zoom] * 1e6, ch1[:n_zoom].imag, label='Channel 1 (Imag)', alpha=0.7)
-ax1.set_title(r'Time Domain: Signal Magnitude (First $500$ samples)')
-ax1.set_xlabel(r'Time ($\mu s$)')
-ax1.set_ylabel(r'Magnitude')
-ax1.legend()
-ax1.grid(True)
 
-# --- Bottom Plot: Frequency Domain (PSD) ---
-ax2.psd(ch1, NFFT=1024, Fs=fs/1e6, label='Channel 1')
-ax2.psd(ch2, NFFT=1024, Fs=fs/1e6, label='Channel 2')
-ax2.set_title(r'Frequency Domain: Power Spectral Density')
-ax2.set_xlabel(r'Frequency ($MHz$)')
-ax2.set_ylabel(r'Power ($dB/Hz$)')
-ax2.legend()
+plt.plot(t[:n_zoom] * 1e6, ch2[:n_zoom].real, label='Channel 2 (real)')
+# ax1.plot(t[:n_zoom] * 1e6, np.abs(ch2[:n_zoom]), label='Channel 2', alpha=0.7)
+plt.plot(t[:n_zoom] * 1e6, ch1[:n_zoom].real, label='Channel 1 (Real)')
+# ax1.plot(t[:n_zoom] * 1e6, ch1[:n_zoom].imag, label='Channel 1 (Imag)', alpha=0.7)
+plt.title(r'Timeplot of first $10000$ samples')
+plt.xlabel(r'Time ($\mu s$)')
+plt.ylabel(r'Magnitude')
+plt.legend()
+plt.grid(True)
+
 
 plt.tight_layout()
-plt.savefig('plot.png', dpi=300, bbox_inches='tight')
+plt.savefig('plot1.png', dpi=300, bbox_inches='tight')
