e51454593b7116820d86721f6714e3c69f16e33a
Pi1 <hclaus23@student.aau.dk>
2026-06-15 22:29:16 +0200

    Throughput tx kode

--- ThroughputTest/ThroughputTX.py
+++ ThroughputTest/ThroughputTX.py
@@ -10,8 +10,8 @@ from lblprof import start_tracing, stop_tracing, show_interactive_tree, show_tre
 IS_PI1 = (gethostname() == "pi1")
 
 threshold = 5
-
-duration = 62 # seconds
+test_num = 0
+duration = 60 # seconds
 
 
 
@@ -30,21 +30,23 @@ while True:
     if IS_PI1:
             data = recv_data(radio, decoder) 
             if data is None:
-                print("No work")
+                #print("No work")
                 continue
             packet,esprit = data
             if packet == "start":
-                print("Starting spam")
+                print(f"Starting spam: {test_num}")
                 sleep(0.1)  # Ensure Pi2 is in recv mode
                 start_time = time()
                 while (time() - start_time) < duration:
                     timestamp = time() - start_time
                     packet_count += 1
-                    print(f"Time: {timestamp} packet: {packet_count}")
+                    #print(f"Time: {timestamp} packet: {packet_count}")
                     #start_tracing()
-                    radio.transmit(str(packet_count),repeat = 1)
+                    radio.transmit(str(packet_count),repeat = 2)
                     #stop_tracing()
                     #show_tree() # print the tree to console
-                print("Stopping spam")
+                print(f"Time: {timestamp} packets: {packet_count}")
+                print(f"Stopping test: {test_num}")
+                test_num += 1
                 packet_count = 0
                 
